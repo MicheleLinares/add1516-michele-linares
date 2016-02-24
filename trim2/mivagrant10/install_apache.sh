d0dfbc31d9ac183741e7e717e7f0a67d133cdbe7
michelelinares <michelelinaresdonofrio@gmail.com>
2016-02-24 08:55:39 +0000

    vagrant

---
+++ trim2/mivagrant10/install_apache.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apache2
+rm -rf /var/www
+ln -fs /vagrant /var/www
+echo "<h1>Actividad de Vagrant</h1>" > /var/www/index.html
+echo "<p>Curso201516</p>" >> /var/www/index.html
+echo "<p>Michele</p>" >> /var/www/index.html
+
+
